66433a54113caf8c43ff9081fa8921bd1eacbc76
Qinali Ma <108873423+MqLeet@users.noreply.github.com>
2023-08-18 13:45:51 +0800

    Update predict.sh

--- predict.sh
+++ predict.sh
@@ -1,6 +1,11 @@
 export CUDA_VISIBLE_DEVICES=0,1
-python3 scripts/predict_for_dir.py hrnet32_idih256 /home/hongyan/dms_gen/image_harmonization/pretrained_models/hrnet32_idih256.pth \
-    --images /data/hongyan/dms_binary_cls_dataset/dms_gen_normal_face/origin \
-    --masks /data/hongyan/dms_binary_cls_dataset/dms_gen_normal_face/matting/alpha \
+python3 scripts/predict_for_dir.py MODEL_NAME PATH/TO/YOUR/MODEL \
+    --images PATH/TO/YOUR/IMAGES \
+    --masks PATH/TO/REGION\'S/MASK/ \
     --resize 768 \
-    --results_path /data/hongyan/dms_binary_cls_dataset/dms_gen_normal_face/dms_gen_harmonization
+    --results_path /PATH/TO/SAVE/RESULT
+
+# 第一行的MODEL_NAME PATH/TO/YOUR/MODEL替换成对应的pretrained model的名字和存储路径
+# images是待处理的图像的路径
+# masks是待和谐话区域的mask
+# results_path是存储和谐化结果的路径
